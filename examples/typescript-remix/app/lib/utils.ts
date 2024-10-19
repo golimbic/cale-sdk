cf59c784302c8d130931e09fcab48c2bb5526361
Limbic <kk@limbic.dev>
2024-10-19 14:57:06 -0500

    fix: utils based on new price type

--- examples/typescript-remix/app/lib/utils.ts
+++ examples/typescript-remix/app/lib/utils.ts
@@ -3,13 +3,13 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
 }
 
 export function formatPrice(price: Price) {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: price.currency,
-  });
-  return formatter.format(price.amount / 100);
+	const formatter = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: price.currency,
+	});
+	return formatter.format(Number(price.amount));
 }
